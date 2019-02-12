948edfb64886b312df7169e77e75486cc21d623d
kevout <kev.gomez97@hotmail.com>
2019-02-11 21:14:07 -0600

    El CRUD se ejecuta recibiendo como parametros el servidor, usuario y contraseña de la base de datos

--- Crud.py
+++ Crud.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
-
+import sys
 # initializations
 app = Flask(__name__)
 
 # Mysql Connection
-app.config['MYSQL_HOST'] = 'localhost' 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_HOST'] = sys.argv[1] 
+app.config['MYSQL_USER'] = sys.argv[2]
+app.config['MYSQL_PASSWORD'] = sys.argv[3]
 app.config['MYSQL_DB'] = 'crud_certificates'
 mysql = MySQL(app)
 
